fb5654150a8a3d92356c8a603b3e866dfb9f3b7a
Ashutosh Bambal <ashutoshbambal97@gmail.com>
2024-07-13 19:58:33 +0530

    test46

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,25 @@
-instance_type = "t2.micro"
-tags          = "ubuntu-machine"
-region        = "us-east-1"
+region      = "us-east-1"
+server_name = ["web-testing", "web-production"]
+ami_id = {
+  "us-east-1" = "ami-0a0e5d9c7acc336f1"
+  "us-west-1" = "ami-0ff591da048329e00"
+}
+environment = "test"
+key_path    = "./id_rsa.pub"
+ports = [
+  {
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  },
+  {
+    from_port   = 80
+    to_port     = 90
+    cidr_blocks = ["192.168.1.0/24"]
+  },
+  {
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["192.168.2.0/24"]
+  }
+]
--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
-variable "instance_type" {
-  description = "Specify the instance type"
-  type        = string
-}
+# variable "instance_type" {
+#   description = "Specify the instance type"
+#   type        = string
+# }
 
-variable "tags" {
-  description = "Provide the tagging to AWS machine"
-  type        = string
-}
+# variable "tags" {
+#   description = "Provide the tagging to AWS machine"
+#   type        = string
+# }
 
 variable "region" {
   description = "AWS region name"
@@ -20,15 +20,11 @@ variable "region" {
 variable "server_name" {
   description = "name of the server that we are going to deploy"
   type        = list(string)
-  #   default     = ["web-testing", "web-production","dev"]
-  default = ["web-testing", "web-dev", "web-production"]
-
 }
 
 variable "environment" {
   description = "Environment Name"
   type        = string
-  default     = "test"
   validation {
     condition     = contains(["test", "prod"], var.environment)
     error_message = "Not goinig to support this environment"
@@ -38,8 +34,18 @@ variable "environment" {
 variable "ami_id" {
   description = "AWS machine AMI"
   type        = map(any)
-  default = {
-    "us-east-1" = "ami-0a0e5d9c7acc336f1"
-    "us-west-1" = "ami-0ff591da048329e00"
+}
+
+variable "key_path" {
+  description = "Your public SSH Key path"
+  type        = string
+  validation {
+    condition     = fileexists(var.key_path)
+    error_message = "Public SSH key not found"
   }
+}
+
+variable "ports" {
+  description = "List of ports objects"
+  type        = any
 }
