c7b8317cb7b6fafea394d7d817d4b19f9029c9e6
Ashutosh Bambal <ashutoshbambal97@gmail.com>
2024-07-13 14:18:06 +0530

    test15

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "instance_type" {
   description = "Specify the instance type"
   type        = string
-  default     = "t2.medium"
 }
 
 variable "tags" {
   description = "Provide the tagging to AWS machine"
   type        = string
-  default     = "web-server"
 }
