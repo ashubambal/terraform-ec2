a43e18ba860f011646a328db2eb46b055ecc42e5
Ashutosh Bambal <ashutoshbambal97@gmail.com>
2024-07-13 15:37:55 +0530

    test23

--- main.tf
+++ main.tf
@@ -1,21 +1,22 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical1
-}
+#   owners = ["099720109477"] # Canonical1
+# }
 
 resource "aws_instance" "web" {
-  ami             = data.aws_ami.ubuntu.id
+  # ami             = data.aws_ami.ubuntu.id
+  ami             = var.ami_id[var.region]
   instance_type   = var.instance_type
   key_name        = aws_key_pair.deployer.id
   security_groups = [aws_security_group.sg-web.name]
--- variables.tf
+++ variables.tf
@@ -13,3 +13,12 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "ami_id" {
+  description = "AWS machine AMI"
+  type        = map(any)
+  default = {
+    "us-east-1" = "ami-0a0e5d9c7acc336f1"
+    "us-west-1" = "ami-0ff591da048329e00"
+  }
+}
