379999c6b48dd9fd6146ce16ab08b02dee188618
Ashutosh Bambal <ashutoshbambal97@gmail.com>
2024-07-14 08:41:26 +0530

    test-8

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "web" {
   }
 
   provisioner "file" {
-    source      = "../ecommerce-website-html-css/"
+    source      = "../src/"
     destination = "/var/www/html/"
   }
 }
